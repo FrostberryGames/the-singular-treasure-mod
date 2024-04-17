d21738a17ac7d1c00494ef8689c3faa1b46bd062
mbeau <bluefoxy2010@gmail.com>
2024-04-16 21:31:26 -0400

    Coolio

---
+++ src/main/java/net/frostberrynstuff/singulartreasure/SingularTreasureClient.java
@@ -0,0 +1,11 @@
+package net.frostberrynstuff.singulartreasure;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SingularTreasureClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/frostberrynstuff/singulartreasure/SingularTreasureDataGenerator.java
@@ -0,0 +1,11 @@
+package net.frostberrynstuff.singulartreasure;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SingularTreasureDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
